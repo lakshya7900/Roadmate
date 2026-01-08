e31a745d82d4ef006fff807ed3a476bbcef981ba
Lakshya Agarwal <aglakshya06@gmail.com>
2026-01-08 14:30:36 -0800

    Initial Commit

---
+++ Roadmate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Roadmate
+//
+//  Created by Lakshya Agarwal on 1/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Roadmate/RoadmateApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoadmateApp.swift
+//  Roadmate
+//
+//  Created by Lakshya Agarwal on 1/8/26.
+//
+
+import SwiftUI
+
+@main
+struct RoadmateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
